16c5f8d86956cb9506fc26492ae3e348931cc0b4
Ferdi <57798860+ImHyper01@users.noreply.github.com>
2024-05-06 15:22:14 +0200

    begin gemaakt aan de navigation bar

---
+++ pages/MapScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Map() {
+    return (
+        <View>
+            <text>Map screen</text>
+        </View>
+    );
+}
